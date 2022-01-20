c023826e9ef8370e6d4ab44a4db1ad30369b3887
CENGIZHAN KARABEL <69279953+cengizhankarabel@users.noreply.github.com>
2022-01-19 23:44:22 -0600

    "Java Exception Handling Try Catch"

---
+++ src/Java_Exception_Handling_Try_Catch/Solution.java
@@ -0,0 +1,67 @@
+package Java_Exception_Handling_Try_Catch;
+
+/*
+
+   ---->  Java Exception Handling Try Catch
+
+Exception handling is the process of responding to the occurrence, during computation, of exceptions – anomalous or exceptional conditions requiring special processing – often changing the normal flow of program execution. (Wikipedia)
+
+Java has built-in mechanism to handle exceptions. Using the try statement we can test a block of code for errors. The catch block contains the code that says what to do if exception occurs.
+
+This problem will test your knowledge on try-catch block.
+
+You will be given two integers  and  as input, you have to compute . If  and  are not  bit signed integers or if  is zero, exception will occur and you have to report it. Read sample Input/Output to know what to report in case of exceptions.
+
+Sample Input 0:
+
+10
+3
+Sample Output 0:
+
+3
+Sample Input 1:
+
+10
+Hello
+Sample Output 1:
+
+java.util.InputMismatchException
+Sample Input 2:
+
+10
+0
+Sample Output 2:
+
+java.lang.ArithmeticException: / by zero
+Sample Input 3:
+
+23.323
+0
+Sample Output 3:
+
+java.util.InputMismatchException
+
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        try{
+            try {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+
+                System.out.println(x / y);
+            }catch (InputMismatchException e) {
+                System.out.println("java.util.InputMismatchException");
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
