370f9e0eefd624fc22e52af82a7d2dbb9e6ab32d
CENGIZHAN KARABEL <69279953+cengizhankarabel@users.noreply.github.com>
2022-01-20 01:14:06 -0600

    "Java Singleton Pattern"

---
+++ src/Java_Singleton_Pattern/Solution.java
@@ -0,0 +1,50 @@
+package Java_Singleton_Pattern;
+
+/*
+
+   ---->  Java Singleton Pattern
+
+"The singleton pattern is a design pattern that restricts the instantiation of a class to one object. This is useful when exactly one object is needed to coordinate actions across the system."
+- Wikipedia: Singleton Pattern
+
+Complete the Singleton class in your editor which contains the following components:
+
+A private Singleton non parameterized constructor.
+A public String instance variable named .
+Write a static method named getSingleInstance that returns the single instance of the Singleton class.
+Once submitted, our hidden Solution class will check your code by taking a String as input and then using your Singleton class to print a line.
+
+Input Format
+
+You will not be handling any input in this challenge.
+
+Output Format
+
+You will not be producing any output in this challenge.
+
+Sample Input
+
+hello world
+Sample Output
+
+Hello I am a singleton! Let me say hello world to you
+
+ */
+
+class Singleton{
+
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+    }
+
+    public String str;
+
+    public static Singleton getSingleInstance() {
+        return singleton;
+    }
+}
+
+public class Solution {
+
+}
